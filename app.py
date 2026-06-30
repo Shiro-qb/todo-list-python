bbf9d7675817dd24ebf42585ada94dc49e1462d3
Shiro-qb <bilanmorat@gmail.com>
2026-06-30 18:24:24 -0400

    add_task update

--- app.py
+++ app.py
@@ -1,6 +1,28 @@
 import tkinter as tk
 from Task import Task
 
+tasks = []
+def add_task():
+    title = entry.get( )
+    if title is None:
+        return
+    task = Task(title, False, "", "")
+    tasks.append(task)
+    listbox.insert(tk.END, task.title)
+    entry.delete(0, tk.END)
+
+def delete_task():
+    selection = listbox.curselection()
+    if not selection:
+        return
+    index = selection[0]
+    tasks.pop(index)
+    listbox.delete(index)
+
+
+
+
+
 
 root = tk.Tk()
 root.title("To Do List")
